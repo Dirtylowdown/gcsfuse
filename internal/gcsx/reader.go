ef3b3dbb586b65fc5b398f61569cc3e4a99577fc
Tulsi Shah <tulsishah@google.com>
2025-04-02 15:22:01 +0000

    test changes

--- internal/gcsx/reader.go
+++ internal/gcsx/reader.go
@@ -21,28 +21,35 @@ import (
 	"github.com/googlecloudplatform/gcsfuse/v2/internal/storage/gcs"
 )
 
-// RandomReader is an object that knows how to read ranges within a particular
-// generation of a particular GCS object. Optimised for (large) sequential reads.
-//
-// Not safe for concurrent access.
-//
-// TODO - (raj-prince) - Rename this with appropriate name as it also started
-// fulfilling the responsibility of reading object's content from cache.
-type Reader interface {
-	// Panic if any internal invariants are violated.
+// Ensures internal consistency of the implementation
+type InvariantChecker interface {
 	CheckInvariants()
+}
 
-	// ReadAt returns the data from the requested offset and upto the size of input
-	// byte array. It either populates input array i.e., p or returns a different
-	// byte array. In case input array is populated, the same array will be returned
-	// as part of response. Hence the callers should use the byte array returned
-	// as part of response always.
+// Provides methods to read data at a specific offset
+type DataReaderWithEnd interface {
 	ReadAt(ctx context.Context, p []byte, offset, end int64) (objectData readers.ObjectData, err error)
+}
 
-	// Return the record for the object to which the reader is bound.
+// Provides methods to read data at a specific offset
+type DataReader interface {
+	ReadAt(ctx context.Context, p []byte, offset int64) (objectData readers.ObjectData, err error)
+}
+
+// Provides access to object metadata
+type ObjectAccessor interface {
 	Object() (o *gcs.MinObject)
+}
 
-	// Clean up any resources associated with the reader, which must not be used
-	// again.
+// Handles cleanup of resources
+type Destroyer interface {
 	Destroy()
+}
+
+// Combines all interfaces for a complete reader
+type Reader interface {
+	InvariantChecker
+	DataReader
+	ObjectAccessor
+	Destroyer
 }
